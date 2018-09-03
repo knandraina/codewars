309ef13df94035e34e38f17917debc9d7cdf7aa1
Kevin Rabesaotra <kevin.rabesaotra@gmail.com>
2018-09-03 10:52:37 +0200

    done

---
+++ 6kyu/build tower.js
@@ -0,0 +1,41 @@
+// Build Tower
+// Build Tower by the following given argument:
+// number of floors (integer and always greater than 0).
+
+// Tower block is represented as *
+
+// Python: return a list;
+// JavaScript: returns an Array;
+// C#: returns a string[];
+// PHP: returns an array;
+// C++: returns a vector<string>;
+// Haskell: returns a [String];
+// Ruby: returns an Array;
+// Have fun!
+
+// for example, a tower of 3 floors looks like below
+
+// [
+//   '  *  ', 
+//   ' *** ', 
+//   '*****'
+// ]
+// and a tower of 6 floors looks like below
+
+// [
+//   '     *     ', 
+//   '    ***    ', 
+//   '   *****   ', 
+//   '  *******  ', 
+//   ' ********* ', 
+//   '***********'
+// ]
+// Go challenge Build Tower Advanced once you have finished this :)
+
+function towerBuilder(nFloors) {
+    var array=[]; 
+      for (var i = 0 ; i < nFloors ; i ++ ) {
+          array.push( " ".repeat( nFloors - i - 1 )+ "*".repeat((i * 2) + 1 )+  " ".repeat( nFloors - i - 1) );
+      }
+    return array  
+    }
